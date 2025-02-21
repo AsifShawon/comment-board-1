78448f1838e15c4062e329a03b206e6f86242dca
AsifShawon <asifbhshawon71@gmail.com>
2025-02-21 15:29:13 +0600

    jani na bhai kichu

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import CommentBoard  from '@/components/comment-board';
-import { QRCodeDisplay } from '@/app/components/qr-code-display';
+// import CommentBoard  from '@/components/comment-board';
+// import { QRCodeDisplay } from '@/app/components/qr-code-display';
 
 export default function Home() {
   return (
     <main className="fixed inset-0 bg-gradient-to-b from-blue-50 to-blue-100 dark:from-blue-900 dark:to-blue-950 overflow-hidden">
+      hello
+    </main>
+  );
+}
+
+{/* <main className="fixed inset-0 bg-gradient-to-b from-blue-50 to-blue-100 dark:from-blue-900 dark:to-blue-950 overflow-hidden">
       <div className="absolute top-4 right-4 z-10 w-96">
-        {/* <QRCodeDisplay /> */}
+        {/* <QRCodeDisplay /> 
       </div>
       <div className="absolute top-4 left-4 z-10">
         <h1 className="text-2xl font-bold text-blue-800 dark:text-blue-200">
@@ -15,6 +21,5 @@ export default function Home() {
       <div className="w-full h-full">
         <CommentBoard />
       </div>
-    </main>
-  );
-}
+    </main> 
+    */}
